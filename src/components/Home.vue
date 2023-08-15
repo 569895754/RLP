fea28cd4b1416e05ddc55f340e4d12e77080195f
569895754Wr <569895754@qq.com>
2023-08-15 22:46:49 +0800

    init object

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="parent">
+        <div class="left">
+            
+        </div>
+        <div class="right">
+            <div>
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+.parent {
+    display: flex;
+}
+.left {
+    width: 280px;
+    height: 100vh;
+    border-right: 1px solid #dfdfdf;
+}
+.right {
+    flex: 1;
+    height: 100%;
+}
+.myButton {
+    height: 30px;
+}
+</style>
---
+++ src/components/directory/List.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="List">
+    </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+.List {
+    margin-top: 40px;
+}
+</style>
